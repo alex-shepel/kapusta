c8a9efa26a2a89a94598c3ba5b3347c666ca8386
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2022-01-14 20:13:53 +0200

    Сделал верстку e-mail-формы

--- src/views/AuthView/AuthView.jsx
+++ src/views/AuthView/AuthView.jsx
@@ -1,7 +1,20 @@
 import PropTypes from 'prop-types';
 
+import AuthorizationWithGoogle from '../../components/AuthorizationWithGoogle';
+import AuthorizationWithEmail from '../../components/AuthorizationWithEmail';
+
 const AuthView = props => {
-  return <div>AuthView</div>;
+  return (
+    <>
+      <div>AuthView</div>
+      <p>Вы можете авторизоваться с помощью Google Account:</p>
+      <AuthorizationWithGoogle />
+      <p>
+        Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
+      </p>
+      <AuthorizationWithEmail />
+    </>
+  );
 };
 
 AuthView.propTypes = {};
