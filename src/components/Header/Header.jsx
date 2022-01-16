6eddf2a7cb4652d2b9f416349eb6b82313771980
bod111 <86015208+bod111@users.noreply.github.com>
2022-01-16 12:52:36 +0200

    stiles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,43 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react'
+import {useSelector} from "react-redux"
+import logo from "images/logo.svg"
+import logout from "images/logout-mobile.svg"
+import {getIsLoggedIn} from "redux/auth"
+import s from "./Header.module.css"
 
-const Header = props => {
+
+
+const Header = () => {
+  const [width, setWidth] = useState(window.innerWidth)
+
+const handleResizeWindow = () => setWidth(window.innerWidth);
+const isLoggedIn = useSelector(getIsLoggedIn)
+  useEffect(() => {
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+const breakPointTablet = 768;
   return (
-    <div className="container">
-    <div></div>
-  </div>)
+    <div className={s.container}>
+      <div className={s.header__wrap}>
+        <img className={s.header__logo} src={logo} alt={"logo"} />
+        {isLoggedIn && (
+          <div className={s.header__nav}>
+            <div className={s.userLogo__circle}>
+              <div className={s.userLogo}>U</div>
+            </div>
+            {width < breakPointTablet ? (<img src={logout} alt= {"logout" }/>) : (<><div>User name</div><div>Выйти</ div></>)}
+        </div>)}
+      </div>
+    </div>)
 };
 
-Header.propTypes = {};
+Header.propTypes = {
+  
+};
 
 export default Header;
