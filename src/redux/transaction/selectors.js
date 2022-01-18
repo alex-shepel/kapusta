b7a8ffd1d4d8011974dda24fac7b49310c77e7b6
Alex Shepel <iam.alex.shepel@gmail.com>
2022-01-18 18:17:29 +0200

    fix transaction reducer

--- src/redux/transaction/selectors.js
+++ src/redux/transaction/selectors.js
@@ -1,16 +1,16 @@
-const getIncomes = state => state.incomes;
-const getExpenses = state => state.expenses;
-const getMonthStatsIncomes = state => state.monthStatsIncomes;
-const getMonthStatsExpenses = state => state.monthStatsExpenses;
-const getIncomeCategories = state => state.categoriesIncome;
-const getExpenseCategories = state => state.categoriesExpense;
+const getIncomes = state => state.transaction.incomes;
+const getExpenses = state => state.transaction.expenses;
+const getMonthStatsIncomes = state => state.transaction.monthStatsIncomes;
+const getMonthStatsExpenses = state => state.transaction.monthStatsExpenses;
+const getIncomeCategories = state => state.transaction.categoriesIncome;
+const getExpenseCategories = state => state.transaction.categoriesExpense;
 
-const getIsIncomeAdding = state => state.isIncomeAdding;
-const getIsIncomeLoading = state => state.isIncomeLoading;
-const getIsExpenseAdding = state => state.isExpenseAdding;
-const getIsExpenseLoading = state => state.isExpenseLoading;
-const getAreCategoriesLoading = state => state.areCategoriesLoading;
-const getIsRemoving = state => state.isRemoving;
+const getIsIncomeAdding = state => state.transaction.isIncomeAdding;
+const getIsIncomeLoading = state => state.transaction.isIncomeLoading;
+const getIsExpenseAdding = state => state.transaction.isExpenseAdding;
+const getIsExpenseLoading = state => state.transaction.isExpenseLoading;
+const getAreCategoriesLoading = state => state.transaction.areCategoriesLoading;
+const getIsRemoving = state => state.transaction.isRemoving;
 
 export {
   getIncomes,
