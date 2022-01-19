0353d2eb8ecc278695001ea80349a11a97f265be
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2022-01-19 14:51:18 +0200

    Добавил верхнюю капусту на мобильной версии

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,7 +1,12 @@
 import s from './Background.module.css';
 
 const Background = () => {
-  return <div className={s.background} />;
+  return (
+    <>
+      <div className={s.background} />
+      <div className={s.backgroundImageTop} />
+    </>
+  );
 };
 
 export default Background;
