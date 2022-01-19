986cd4edd6c61697452420353dd1f3dff0016f2e
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2022-01-19 15:38:04 +0200

    Удалил неверные изображения и исправил отображение фона, который выходит за границы экрана

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,9 +3,10 @@ import s from './Background.module.css';
 const Background = () => {
   return (
     <>
-      <div className={s.background} />
-      <div className={s.backgroundImageTop} />
-      <div className={s.backgroundImageBot} />
+      <div className={s.background}>
+        <div className={s.backgroundImageTop} />
+        <div className={s.backgroundImageBot} />
+      </div>
     </>
   );
 };
