d9d48aa1de087d877bbc3721b7141ca81c072aa4
Subm1s <82820375+Subm1s@users.noreply.github.com>
2022-01-19 18:42:20 +0200

    month-report

--- src/components/Report/ReportExpencesList/ReportExpencesList.jsx
+++ src/components/Report/ReportExpencesList/ReportExpencesList.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import sprite from '../icon.svg';
 import category from './expence.json';
 import s from './ReportExpencesList.module.css';
 
+
 const ReportExpencesList = () => {
+
+  
+
   return (
     <ul className={s.expenceReport}>
       {category.map(el => (
-        <li key={el.id} className={s.expenceReportItem}>
+        <li key={el.id} onClick={() => (el.label)} className={s.expenceReportItem}>
           <p className={s.expenceValue}>500.00</p>
           <svg className={s.expenceIcon}>
             <use xlinkHref={`${sprite}#${el.label}`} />
