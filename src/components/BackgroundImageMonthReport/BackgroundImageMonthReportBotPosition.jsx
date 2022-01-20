481141740a7c243146f5e0a417a7ae58a1f356b9
Alex Shepel <79785130+alex-shepel@users.noreply.github.com>
2022-01-20 14:37:53 +0200

    Update BackgroundImageMonthReportBotPosition.jsx

--- src/components/BackgroundImageMonthReport/BackgroundImageMonthReportBotPosition.jsx
+++ src/components/BackgroundImageMonthReport/BackgroundImageMonthReportBotPosition.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import s from 'components/BackgroundImageMonthReport/BackgroundImageMonthReportBotPosition.module.css';
 
 const BackgroundImageMonthReportBotPosition = () => {
