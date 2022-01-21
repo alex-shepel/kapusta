268c3d60579516d52766e7a8ea7c9c456f569aa6
bod111 <86015208+bod111@users.noreply.github.com>
2022-01-21 23:12:38 +0200

    fix spinner position

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import { Oval } from 'react-loader-spinner';
 import PropTypes from 'prop-types';
 import s from './Spinner.module.css';
 
-const Spinner = ({ size = 70, color = '#000' }) => {
+const Spinner = ({ size = 70, color = '#3e6183' }) => {
   return (
     <div className={s.spinner}>
       <Oval color={color} height={`${size}`} width={`${size}`} />
